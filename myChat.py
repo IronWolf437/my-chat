fc81ed88a6c480307d7af290b102cbfdd8024a3d
Iron Wolf <woiron437@gmail.com>
2026-01-16 00:50:23 +0200

    Enhance user input validation and chat file management
    Added error handling for user number input and improved file handling for chat title.

--- myChat.py
+++ myChat.py
@@ -1,16 +1,36 @@
 while True:
-    userNumber = input('how number of users: ')
+    # Get number of users & their names
+    try:
+        userNumber = int(input('how number of users: '))
+    except:
+        print('\033[0;31mPlease enter a valid numbe\033[0m')
+        continue
+
     users = []
-    
-    for i in range(int(userNumber)):
+
+    for i in range(userNumber):
         user = input('whoAreYou: ')
         users.append(user)
 
-    print('')
 
+    # Get chat title & create/open chat file
+    chatTitle = input('chatTitle: ')
+    with open(f'{chatTitle}.txt', 'a', encoding='utf-8') as file:
+        file
+
+    print(f'\n=== {chatTitle} ===\n')
+
+    with open(f'{chatTitle}.txt', 'r', encoding='utf-8') as file:
+        print(file.read())
+
+
+    # Chat loop
     while True:
         for user in users: 
             message = input(f"[{user}]: ")
 
-            with open('chat.txt', 'a', encoding='utf-8') as file:
-                file.write(f'{user}: {message}\n')
+            if message == 'exit':
+                exit()
+            else:
+                with open(f'{chatTitle}.txt', 'a', encoding='utf-8') as file:
+                    file.write(f'{user}: {message}\n')
